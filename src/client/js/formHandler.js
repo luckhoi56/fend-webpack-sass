cce8a1d748ba08ff5acc03646880e0acb8bec2be
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-03-05 16:08:23 -0500

    NLP start here

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,13 +7,14 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=91763&appid=750c257ad74cd91d9a1d54e0c739b736')
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        document.getElementById('results').innerHTML = "hey " + data.main.temp;
     })
 }
 
 export { handleSubmit }
+
